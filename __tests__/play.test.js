d96515b3302b055cfa4ce67d53a8bc9a443a9cbc
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-08-05 15:34:51 -0700

    play tests setup

--- __tests__/play.test.js
+++ __tests__/play.test.js
@@ -1,1 +1,21 @@
-test.todo('should ??');
+const { play, nextPlayer } = require('../lib/play');
+let { getValidMoveFromPlayer } = require('../lib/move');
+const ui = require('../lib/ui');
+
+describe('play', () => {
+  getValidMoveFromPlayer = jest.fn();
+  jest.mock(ui);
+
+  afterEach(() => {
+    getValidMoveFromPlayer.mockClear();
+    ui.displayTurn.mockClear();
+    ui.displayWin.mockClear();
+    ui.displayTie.mockClear();
+  });
+
+  test('should print out a win if there is a winning move', () => {});
+  test('should print out a tie if there is tie game', () => {});
+  test('should keep running if theres no winner or tie', () => {});
+});
+
+describe('nextPlayer', () => {});
